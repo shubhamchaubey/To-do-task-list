eba2c86657942586acceb6e0b5e5056fdfd80b3e
Shubham Chaubey <shubhamchaubey2211@gmail.com>
2022-07-24 21:30:38 +0530

    modified:   app.js 	deleted:    index._oldhtml

--- app.js
+++ app.js
@@ -9,7 +9,14 @@ const taskInput = document.querySelector('#task');
 loadEventListeners();
 
 function loadEventListeners(){
+    //Add task 
     form.addEventListener('submit', addTask);
+    // Remove task
+    taskList.addEventListener('click', removeTask);
+    // CLear task list
+    clearBtn.addEventListener('click', clearTask);
+    // Filter tasks events
+    filter.addEventListener('keyup', filterTasks);
 }
 
 function addTask(e){
@@ -36,4 +43,30 @@ function addTask(e){
 
     taskInput.value='';
     e.preventDefault();
+}
+
+function removeTask(e){
+    if(e.target.parentElement.classList.contains('delete-item')){
+        if(confirm('Are you sure?')){
+        e.target.parentElement.parentElement.remove()
+        }
+    }
+}
+
+function clearTask(){
+    taskList.innerHTML='';
+}
+
+function filterTasks(e){
+    const text = e.target.value.toLowerCase();
+
+    document.querySelectorAll('.collection-item').forEach(function(task){
+        const item = task.firstChild.textContent;
+        if(item.toLowerCase().indexOf(text) != -1){
+            task.style.display = 'block';
+        } else {
+            task.style.display = 'none';
+        }
+    });
+
 }
